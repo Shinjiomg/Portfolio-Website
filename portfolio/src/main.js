c9ecb7383966f7b9c55b03c8a283fc74981257fa
Jhonatan Becerra <yonkitas9@gmail.com>
2024-06-29 14:59:36 -0500

    Vue - Tailwind configuración global

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import Portfolio from './Portfolio.vue'
 
-createApp(App).mount('#app')
+createApp(Portfolio).mount('#portfolio')
