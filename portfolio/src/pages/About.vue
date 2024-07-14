419c98719365c6ca2cfd2bfb6e47b47d4dbc55a5
Jhonatan Becerra <yonkitas9@gmail.com>
2024-07-14 17:25:51 -0500

    fix

--- portfolio/src/pages/About.vue
+++ portfolio/src/pages/About.vue
@@ -4,7 +4,7 @@
             <h1 class="text-4xl font-roboto-slab font-black mr-4 whitespace-nowrap">{{ $t('about.me') }}</h1>
             <div class="w-1/3 h-1 bg-blue-500"></div>
         </div>
-        <p class="xl:text-lg font-montserrat font-medium mb-6 text-justify">{{ $t('about.content') }}</p>
+        <p class="xl:text-lg font-montserrat font-medium mb-6">{{ $t('about.content') }}</p>
         <h2 class="text-3xl font-roboto-slab font-medium mb-6">{{ $t('about.what-i-do') }}</h2>
         <div class="grid grid-cols-1 xl:grid-cols-2 gap-6">
             <div v-for="(card, index) in jobCards" :key="index"
